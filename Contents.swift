375a916418592d42a11189ca1996e2ab3b6aac59
Bill Skrzypczak <bill@webbuildersconsulting.com>
2016-07-05 11:29:23 -0500

    Data Types

---
+++ Contents.swift
@@ -0,0 +1,43 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var myFirstVariable = 42
+
+myFirstVariable = 55
+
+let myFirstConstant = 77
+
+print(myFirstVariable)
+
+print(myFirstConstant, terminator:"")
+
+let mySecondConstant:Double = 99.00
+
+let myThirdConstant:Float64 = 99.76543298
+
+let label = "The answer to the problem is:"
+
+let answer = 234
+
+let widthLabel = label + String(answer)
+
+print(widthLabel)
+
+// bla bla bla
+
+/*
+ 
+ even more bla bla
+ 
+ */
+
+let a = 1; let b = 2;
+
+print(a,b)
+
+
+
+
+
+
